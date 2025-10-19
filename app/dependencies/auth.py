a2ade76cd8bf406679f450ae6ce52144300d2eef
dagi <dagiteferi2011@gmail.com>
2025-10-19 15:26:10 +0300

    fix auth issue

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -5,7 +5,7 @@ import httpx
 from app.config import settings
 from app.utils.retry import async_retry
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://localhost:8004/api/v1/auth/login")
 
 @async_retry()
 async def get_user_data(token: str):
